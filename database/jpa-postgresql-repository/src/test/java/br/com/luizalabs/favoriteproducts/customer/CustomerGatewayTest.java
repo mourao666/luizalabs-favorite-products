379b91a76825672af77dae2bfea76af16b23bb2f
Antonio Mourão <antonio.oliveira@socialbank.com.br>
2021-10-16 22:44:10 -0300

    Adding postgres database gateways tests

---
+++ database/jpa-postgresql-repository/src/test/java/br/com/luizalabs/favoriteproducts/customer/CustomerGatewayTest.java
@@ -0,0 +1,128 @@
+package br.com.luizalabs.favoriteproducts.customer;
+
+import br.com.luizalabs.favoriteproducts.customer.domain.Customer;
+import br.com.luizalabs.favoriteproducts.customer.domain.CustomerStatus;
+import br.com.luizalabs.favoriteproducts.customer.domain.vo.CustomerId;
+import br.com.luizalabs.favoriteproducts.customer.entity.CustomerEntity;
+import br.com.luizalabs.favoriteproducts.customer.entity.CustomerEntityStatus;
+import br.com.luizalabs.favoriteproducts.customer.repository.CustomerEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerGatewayTest {
+
+    private static final String STRING_ID = "123e4567-a456-42d3-e89b-556642440000";
+    private static final String EMAIL = "dummy@testmail.com";
+
+    private static final UUID ID = UUID.fromString(STRING_ID);
+    private static final CustomerId CUSTOMER_ID = CustomerId.from(STRING_ID);
+
+    private static final Customer CUSTOMER = new Customer(CUSTOMER_ID, "Dummy", EMAIL, CustomerStatus.ACTIVE);
+    private static final CustomerEntity CUSTOMER_ENTITY = new CustomerEntity(ID, "Dummy", EMAIL, CustomerEntityStatus.ACTIVE, null);
+
+    @Mock
+    private CustomerEntityRepository repository;
+
+    @InjectMocks
+    private CustomerGateway customerGateway;
+
+    @Test
+    public void whenCreateOrUpdateThenCallRepository() {
+
+        when(repository.save(any(CustomerEntity.class))).thenReturn(CUSTOMER_ENTITY);
+        Customer customer = customerGateway.createOrUpdate(CUSTOMER);
+        assertNotNull(customer);
+
+        ArgumentCaptor<CustomerEntity> captor = ArgumentCaptor.forClass(CustomerEntity.class);
+        verify(repository).save(captor.capture());
+        verifyNoMoreInteractions(repository);
+
+        CustomerEntity customerEntity = captor.getValue();
+        assertNotNull(customerEntity);
+        assertEquals(customer.getId().value(), customerEntity.getId());
+    }
+
+    @Test
+    public void whenDeleteThenCallRepository() {
+        customerGateway.delete(CUSTOMER_ID);
+        verify(repository).deleteById(ID);
+        verifyNoMoreInteractions(repository);
+    }
+
+
+    @Test
+    public void whenFindThenCallRepository() {
+
+        when(repository.findById(ID)).thenReturn(Optional.of(CUSTOMER_ENTITY));
+        Optional<Customer> optionalCustomer = customerGateway.find(CUSTOMER_ID);
+
+        assertTrue(optionalCustomer.isPresent());
+        assertEquals(ID, optionalCustomer.get().getId().value());
+
+        verify(repository).findById(ID);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    public void whenExistsThenCallRepository() {
+
+        when(repository.existsById(ID)).thenReturn(true);
+        boolean result = customerGateway.exists(CUSTOMER_ID);
+
+        assertTrue(result);
+
+        verify(repository).existsById(ID);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    public void whenDoNotExistsThenCallRepository() {
+
+        when(repository.existsById(ID)).thenReturn(false);
+        boolean result = customerGateway.exists(CUSTOMER_ID);
+
+        assertFalse(result);
+
+        verify(repository).existsById(ID);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    public void whenEmailAlreadyExistsThenCallRepository() {
+
+        when(repository.existsByEmail(EMAIL)).thenReturn(true);
+        boolean result = customerGateway.emailAlreadyExists(EMAIL);
+
+        assertTrue(result);
+
+        verify(repository).existsByEmail(EMAIL);
+        verifyNoMoreInteractions(repository);
+    }
+
+    @Test
+    public void whenEmailDoesNotExistsThenCallRepository() {
+
+        when(repository.existsByEmail(EMAIL)).thenReturn(false);
+        boolean result = customerGateway.emailAlreadyExists(EMAIL);
+
+        assertFalse(result);
+
+        verify(repository).existsByEmail(EMAIL);
+        verifyNoMoreInteractions(repository);
+    }
+}
---
+++ database/jpa-postgresql-repository/src/test/java/br/com/luizalabs/favoriteproducts/product/ProductGatewayTest.java
@@ -0,0 +1,123 @@
+package br.com.luizalabs.favoriteproducts.product;
+
+import br.com.luizalabs.favoriteproducts.customer.domain.vo.CustomerId;
+import br.com.luizalabs.favoriteproducts.customer.entity.CustomerEntity;
+import br.com.luizalabs.favoriteproducts.customer.entity.CustomerEntityStatus;
+import br.com.luizalabs.favoriteproducts.customer.repository.CustomerEntityRepository;
+import br.com.luizalabs.favoriteproducts.product.domain.Product;
+import br.com.luizalabs.favoriteproducts.product.domain.vo.ProductId;
+import br.com.luizalabs.favoriteproducts.product.domain.vo.ProductPrice;
+import br.com.luizalabs.favoriteproducts.product.domain.vo.ProductReviewScore;
+import br.com.luizalabs.favoriteproducts.product.entity.ProductEntity;
+import br.com.luizalabs.favoriteproducts.product.repository.ProductEntityRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductGatewayTest {
+
+    private static final String CUSTOMER_STRING_ID = "123e4567-a456-42d3-e89b-556642440000";
+    private static final String PRODUCT_STRING_ID = "123e4567-42d3-a456-e89b-556642440000";
+
+    private static final UUID CUSTOMER_UUID = UUID.fromString(CUSTOMER_STRING_ID);
+    private static final UUID PRODUCT_UUID = UUID.fromString(PRODUCT_STRING_ID);
+
+    private static final CustomerId CUSTOMER_ID = CustomerId.from(CUSTOMER_STRING_ID);
+    private static final ProductId PRODUCT_ID = ProductId.from(PRODUCT_STRING_ID);
+
+    private static final CustomerEntity CUSTOMER_ENTITY = new CustomerEntity(CUSTOMER_ID.value(), "Dummy", "dummy@testmail.com", CustomerEntityStatus.ACTIVE, null);
+
+    private static final Product PRODUCT = new Product(PRODUCT_ID, "Ico & Shadow Of The Colossus para PS3", "sony", ProductPrice.from(79.0d), "http://challenge-api.luizalabs.com/images/b5116953-dbe5-7edc-c72e-377319e21d56.jpg", ProductReviewScore.from(5.0d));
+    private static final ProductEntity PRODUCT_ENTITY = new ProductEntity(PRODUCT_UUID, CUSTOMER_ENTITY, "Ico & Shadow Of The Colossus para PS3", "sony", BigDecimal.valueOf(79.0d), "http://challenge-api.luizalabs.com/images/b5116953-dbe5-7edc-c72e-377319e21d56.jpg", 5.0d);
+
+    static {
+        CUSTOMER_ENTITY.setProducts(Set.of(PRODUCT_ENTITY));
+    }
+
+    @Mock
+    private CustomerEntityRepository customerRepository;
+
+    @Mock
+    private ProductEntityRepository productRepository;
+
+    @InjectMocks
+    private ProductGateway productGateway;
+
+    @Test
+    public void whenAddThenCallRepository() {
+
+        when(customerRepository.getById(any(UUID.class))).thenReturn(CUSTOMER_ENTITY);
+        when(productRepository.save(any(ProductEntity.class))).thenReturn(PRODUCT_ENTITY);
+
+        productGateway.add(PRODUCT, CUSTOMER_ID);
+
+        ArgumentCaptor<ProductEntity> captor = ArgumentCaptor.forClass(ProductEntity.class);
+        verify(productRepository).save(captor.capture());
+        verifyNoMoreInteractions(productRepository);
+
+        ProductEntity productEntity = captor.getValue();
+        assertNotNull(productEntity);
+        assertEquals(PRODUCT.getId().value(), productEntity.getId());
+    }
+
+    @Test
+    public void whenRemoveThenCallRepository() {
+        productGateway.remove(PRODUCT_ID, CUSTOMER_ID);
+        verify(productRepository).deleteByIdAndCustomerId(PRODUCT_ID.value(), CUSTOMER_ID.value());
+        verifyNoMoreInteractions(productRepository);
+    }
+
+    @Test
+    public void whenAlreadyBeenAddedThenCallRepository() {
+
+        when(productRepository.findByIdAndCustomerId(PRODUCT_UUID, CUSTOMER_UUID)).thenReturn(Optional.of(PRODUCT_ENTITY));
+        boolean result = productGateway.alreadyBeenAdded(PRODUCT_ID, CUSTOMER_ID);
+
+        assertTrue(result);
+
+        verify(productRepository).findByIdAndCustomerId(PRODUCT_UUID, CUSTOMER_UUID);
+        verifyNoMoreInteractions(productRepository);
+    }
+
+    @Test
+    public void whenNotBeenAddedThenCallRepository() {
+
+        when(productRepository.findByIdAndCustomerId(PRODUCT_UUID, CUSTOMER_UUID)).thenReturn(Optional.empty());
+        boolean result = productGateway.alreadyBeenAdded(PRODUCT_ID, CUSTOMER_ID);
+
+        assertFalse(result);
+
+        verify(productRepository).findByIdAndCustomerId(PRODUCT_UUID, CUSTOMER_UUID);
+        verifyNoMoreInteractions(productRepository);
+    }
+
+    @Test
+    public void whenSearchThenCallRepository() {
+
+        when(customerRepository.getById(CUSTOMER_UUID)).thenReturn(CUSTOMER_ENTITY);
+        Set<Product> products = productGateway.search(CUSTOMER_ID);
+
+        assertNotNull(products);
+        assertEquals(1, products.size());
+        assertEquals(PRODUCT_ID, ((Product) products.toArray()[0]).getId());
+
+        verify(customerRepository).getById(CUSTOMER_UUID);
+        verifyNoMoreInteractions(customerRepository);
+    }
+}
